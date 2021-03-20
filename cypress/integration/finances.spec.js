dbcf78c1eef1f6e549d0e0bb48d1b6f77aa2b1ce
Vinicius Bilhar Antoniolli <vinicius.antoniolli@compasso.com.br>
2021-03-20 15:11:16 -0300

    optimazing tests and experimenting new features

--- cypress/integration/finances.spec.js
+++ cypress/integration/finances.spec.js
@@ -1,5 +1,7 @@
 /// <reference types ="cypress" />
 
+import {format, prepareLocalStorage} from '../support/utils'
+
 // cy.viewport
 // arquivos de config
 //congis por linha de comando
@@ -14,24 +16,21 @@ context('Dev Finances Agilizei', () => {
     // afterEach -> depois de cada teste
 
     beforeEach(() => {
-        cy.visit('https://devfinance-agilizei.netlify.app/')
-        cy.get('#data-table tbody tr').should('have.length', 0)
+        cy.visit('https://devfinance-agilizei.netlify.app/', {
+            onBeforeLoad: (win) => {
+                prepareLocalStorage(win)
+            }
+        })
     });
 
     it('Cadastrar entradas', () => {
-        // - entender o fluxo manualmente
-        // - mapear os elementos que vamos interagir
-        // - descrever as interações com o cypress
-        // - adicionar as asserções que precisamos
-
         cy.get('#transaction .button').click() // id + classe
         cy.get('#description').type('Mesada')  // id
         cy.get('[name=amount]').type(12) // atributos
         cy.get('[type=date]').type('2021-03-17') // atributos
         cy.get('button').contains('Salvar').click() // tipo e valor
-        
-        cy.get('#data-table tbody tr').should('have.length', 1)      
 
+        cy.get('#data-table tbody tr').should('have.length', 3)      
     });
 
     it('Cadastrar saídas', () => {
@@ -41,40 +40,50 @@ context('Dev Finances Agilizei', () => {
         cy.get('[type=date]').type('2021-03-17') // atributos
         cy.get('button').contains('Salvar').click() // tipo e valor
         
-        cy.get('#data-table tbody tr').should('have.length', 1)
+        cy.get('#data-table tbody tr').should('have.length', 3)
     });
 
     it('Remover entradas e saídas', () => {
-        const entrada = 'Mesada'
-        const saida = 'KinderOvo'
-
-        cy.get('#transaction .button').click() // id + classe
-        cy.get('#description').type(entrada)  // id
-        cy.get('[name=amount]').type(-12) // atributos
-        cy.get('[type=date]').type('2021-03-17') // atributos
-        cy.get('button').contains('Salvar').click() // tipo e valor
-
-        cy.get('#transaction .button').click() // id + classe
-        cy.get('#description').type(saida)  // id
-        cy.get('[name=amount]').type(-12) // atributos
-        cy.get('[type=date]').type('2021-03-17') // atributos
-        cy.get('button').contains('Salvar').click() // tipo e valor
-        
         // estratégia 1: voltar para o elemento pai, e avançar para um td img attr
         cy.get('td.description')
-        cy.contains(entrada)
+        cy.contains('Mesada')
             .parent()
             .find('img[onclick*=remove]')
             .click()
 
         // estratégia 2: buscar todos os irmãos, e buscar o que tem img + attr
         cy.get('td.description')
-            .contains(saida)
+            .contains('Suco Kapo')
             .siblings()
             .children('img[onclick*=remove]')
             .click()
 
         cy.get('#data-table tbody tr').should('have.length', 0)
+    });
+
+    it('Validar saldo com diversas transações', () => {
+        let incomes = 0
+        let expenses = 0
+
+        cy.get('#data-table tbody tr')
+            .each(($el, index, $list) => {
+                cy.get($el).find('td.income, td.expense').invoke('text').then(text => {
+                    if(text.includes('-')){
+                        expenses = expenses + format(text)
+                    } else {
+                        incomes = incomes + format(text)
+                    }
+                })
+
+            })
+
+            cy.get('#totalDisplay').invoke('text').then(text => {
+               
+                let formattedTotalDisplay = format(text)
+                let expectedTotal = incomes + expenses
+
+                expect(formattedTotalDisplay).to.eq(expectedTotal)
+            })
     });
 
 });
